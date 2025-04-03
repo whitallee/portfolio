fa86fec0f778fd80eba8c150e5870d33c4b05d79
Whit Allee <whitallee@gmail.com>
2025-04-03 18:40:17 -0500

    optimized img on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import whitPic from "../public/whit_pic.png";
+
 export default function Home() {
   return (
     <main className="h-screen max-h-screen flex items-center overflow-hidden gap-4 pl-4 pr-4">
       <div className="flex flex-col gap-4">
         <div className="rounded-full overflow-hidden h-[150px] w-[150px] sm:h-[200px] sm:w-[200px]">
-          <Image src={"/whit_pic.png"} alt="Whit Allee, software engineer, programmer, coder" width={300} height={400}/>
+          <Image className="object-cover" src={whitPic} alt="Whit Allee, software engineer, programmer, coder" width={300} height={400} placeholder="blur" priority/>
         </div>
         <h1 className="text-3xl sm:text-6xl">Whit Allee</h1>
         <h2 className="text-xl sm:text-3xl">Full Stack Web Developer<span className="animate-cursor-blink text-teal-800">|</span></h2>
