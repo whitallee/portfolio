71c46b234447118689d6a6f3bd958d3f9bba3013
Whit Allee <whitallee@gmail.com>
2025-04-03 18:32:28 -0500

    updated linkedin contact link added picture to the home page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
             <span className="text-teal-700">.socials <span className="text-white">&#10100;</span></span>
             <span className="indent-12 pb-4">email: <Link href="mailto:whitallee@gmail.com" className="text-teal-400 text-glow">whitallee@gmail.com</Link></span>
             <span className="indent-12">github: <Link href="https://github.com/whitallee" className="text-teal-400 text-glow">whitallee</Link></span>
-            <span className="indent-12 pb-4">linkedin: <Link href="https://www.linkedin.com/in/whit-allee-web-developer/" className="text-teal-400 text-glow">whit-allee-web-developer</Link></span>
+            <span className="indent-12 pb-4">linkedin: <Link href="https://www.linkedin.com/in/swe/" className="text-teal-400 text-glow">whit-swe</Link></span>
             <span className="indent-12">x/twitter: <Link href="https://x.com/AlleeWhit" className="text-teal-400 text-glow">@AlleeWhit</Link></span>
             <span className="indent-12">instagram: <Link href="https://www.instagram.com/whit_allee/" className="text-teal-400 text-glow">@whit_allee</Link></span>
             <span className="text-white">&#10101;</span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="h-screen max-h-screen flex items-center overflow-hidden gap-4 pl-4 pr-4">
       <div className="flex flex-col gap-4">
+        <div className="rounded-full overflow-hidden h-[150px] w-[150px] sm:h-[200px] sm:w-[200px]">
+          <Image src={"/whit_pic.png"} alt="Whit Allee, software engineer, programmer, coder" width={300} height={400}/>
+        </div>
         <h1 className="text-3xl sm:text-6xl">Whit Allee</h1>
         <h2 className="text-xl sm:text-3xl">Full Stack Web Developer<span className="animate-cursor-blink text-teal-800">|</span></h2>
         <div className="py-2 flex flex-col sm:flex-row gap-4">
